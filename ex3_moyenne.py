e1255194b77b2a91b3c4bb639e771229d907b5f9
kaizen <bongwe7juste.email@gmail.com>
2026-09-22 22:24:32 +0200

    feat: ajout de la fonction de calcul de moyenne

---
+++ ex3_moyenne.py
@@ -0,0 +1,8 @@
+def calculer_moyenne(notes):
+    if not notes:
+        return 0
+    return sum(notes) / len(notes)
+mes_notes = [14, 16, 12, 18, 10]
+moyenne = calculer_moyenne(mes_notes)
+print(f"La moyenne des notes est : {moyenne}")
+    
